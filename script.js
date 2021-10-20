85a0474cea2025d9c6cc3cd635ef7abac9ef7a47
Lakshmipriya-K-P <86197354+Lakshmipriya-K-P@users.noreply.github.com>
2021-10-20 14:31:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function changecontent1()
+{
+    let x=document.getElementById("open-this1").style;
+    
+    if(x.display=="block"){
+        x.display="none";
+    }
+    else{
+        x.display="block";
+    }
+}
+
+function changecontent2()
+{
+    let x=document.getElementById("open-this2").style;
+    
+    if(x.display=="block"){
+        x.display="none";
+    }
+    else{
+        x.display="block";
+    }
+}
